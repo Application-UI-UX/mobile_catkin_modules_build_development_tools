04521570f1f55c20d1c8ca78d6eb5eff53871e2d
Daniel Stonier <d.stonier@gmail.com>
2013-05-31 10:27:01 +0900

    catkin init and create scripts.

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rosjava_tools'],
     package_dir={'': 'src'},
-    #scripts=['scripts/android_generate_project_properties',
-    #         'scripts/catkin_init_android_repo',
-    #         'scripts/catkin_create_android_pkg',
-    #         'scripts/catkin_create_android_library_pkg'
-    #        ],
-    #package_data = {'rosjava_tools': [
-    #       'gradle/*',
-    #       'gradle/gradle/wrapper/*'
-    #       'templates/android_package/*',
-    #       'templates/init_repo/*',
-    #    ]},
+    scripts=['scripts/catkin_init_android_repo',
+             'scripts/catkin_create_android_pkg',
+             'scripts/catkin_create_android_library_pkg'
+            ],
+    package_data = {'rosjava_tools': [
+           'gradle/*',
+           'gradle/gradle/wrapper/*'
+           'templates/android_package/*',
+           'templates/init_repo/*',
+        ]},
     requires=['rospy' 'rospkg']
 )
 
