6942ed58af0ea0e80613ece3d4bcf2506a260a5c
Daniel Stonier <d.stonier@gmail.com>
2013-09-17 16:55:03 +0900

    make sure variables are generated for sh shells as well - catkin uses these.

--- env-hooks/15.rosjava.sh.em
+++ env-hooks/15.rosjava.sh.em
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 @[if DEVELSPACE]@
 export ROS_MAVEN_PATH=`python @(CMAKE_CURRENT_SOURCE_DIR)/generate_environment_variables.py --maven-path`
@@ -9,4 +9,3 @@ mkdir -p ${GRADLE_USER_HOME}
 export ROS_MAVEN_PATH=`python @(CMAKE_INSTALL_PREFIX)/share/rosjava_build_tools/generate_environment_variables.py --maven-path`
 export ROS_MAVEN_DEPLOYMENT_PATH=`python @(CMAKE_INSTALL_PREFIX)/share/rosjava_build_tools/generate_environment_variables.py --maven-deployment-repository`
 @[end if]@
-
--- generate_environment_variables.py
+++ generate_environment_variables.py
@@ -7,7 +7,10 @@ CATKIN_MARKER_FILE = '.catkin'
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate environment variables for the rosjava maven environment.')
-    parser.add_argument('-d', '--deployment-repository', action='store_true', help='Return the current devel workspace maven directory.')
+    cmd_group = parser.add_mutually_exclusive_group()
+    cmd_group.add_argument('-d', '--maven-deployment-repository', action='store_true', help='Return the current devel workspace maven directory.')
+    cmd_group.add_argument('-m', '--maven-path', action='store_true', help='Generate maven path across all chained workspcaes.')
+    cmd_group.add_argument('-g', '--gradle-user-home', action='store_true', help='Generate the local gradle user home in the current devel workspace (share/gradle).')
     args = parser.parse_args()
     return args
 
@@ -26,9 +29,14 @@ def get_workspaces(environ):
 if __name__ == '__main__':
     args = parse_arguments()
     workspaces = get_workspaces(dict(os.environ))
-    if args.deployment_repository:
+    if args.maven_deployment_repository:
         # assuming one value exists here
         print os.path.join(workspaces[0], 'share', 'maven')
-    else:
+    elif args.maven_path:
         maven_repository_paths = [os.path.join(path, 'share', 'maven') for path in workspaces]
         print os.pathsep.join(maven_repository_paths)
+    elif args.gradle_user_home:
+        # assuming one value exists here
+        print os.path.join(workspaces[0], 'share', 'gradle')
+    else:
+        print "Nothing to see here - please provide one of the valid command switches."
