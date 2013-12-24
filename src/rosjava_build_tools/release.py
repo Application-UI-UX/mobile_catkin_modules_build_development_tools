413e809c6ee3fe105f570a7172ce43e6e09f3477
Daniel Stonier <d.stonier@gmail.com>
2013-12-24 09:57:03 +0900

    update jenkins reindexing scripts.

--- src/rosjava_build_tools/release.py
+++ src/rosjava_build_tools/release.py
@@ -4,30 +4,37 @@
 # Imports
 ##############################################################################
 
-from rosinstall_generator.generator import ARG_ALL_PACKAGES, generate_rosinstall, sort_rosinstall
+import rosdistro
+import catkin_pkg
 
 ##############################################################################
 # Imports
 ##############################################################################
 
 
+def has_build_depend_on_message_generation(package):
+    '''
+      Checks for a build dependency on message generation to determine if
+      that package contains msgs/srvs.
+
+      @param package : typical catkin package object
+      @type catkin_pkg.Package
+
+      @return True if it is a package that contains msgs/srvs
+      @rtype Bool
+    '''
+    return 'message_generation' in [d.name for d in package.build_depends]
+
+
 def scrape_for_release_message_packages(track):
-    try:
-        # Should use ROS_DISTRO here, or some passed in value.
-        rosinstall_data = generate_rosinstall(track, [ARG_ALL_PACKAGES],
-            wet_only=True, dry_only=False
-            )
-    except RuntimeError as unused_e:
-        raise RuntimeError("error occured while scraping rosdistro for msg package naems and versions.")
-    rosinstall_data = sort_rosinstall(rosinstall_data)
-    #print("%s" % rosinstall_data)
+    url = rosdistro.get_index_url()
+    index = rosdistro.get_index(url)
+    cache = rosdistro.get_release_cache(index, 'hydro')
     packages = []
-    for element in rosinstall_data:
-        for unused_key, value in element.items():
-            if "_msgs" in value['local-name'] or "_srvs" in value['local-name']:
-                name = value['local-name']
-                # bloom version is usually of the form: 'release/hydro/zeroconf_msgs/0.2.1-0'
-                version = value['version'].split('/')[-1].split('-')[0]
-                pkg = {'name': name, 'version': version}
-                packages.append(pkg)
+    for package_name, package_string in cache.package_xmls.iteritems():
+        package = catkin_pkg.package.parse_package_string(package_string)
+        #print("  Name: %s" % package_name)
+        #print("  Buildtool Depends %s" % package.build)
+        if has_build_depend_on_message_generation(package):
+            packages.append({'name': package_name, 'version': package.version})
     return packages
