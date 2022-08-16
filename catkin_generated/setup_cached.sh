10865f2c39f42560bf64addf118071e16804a55d
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-16 09:45:59 -0400

    build

---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY=''
+export ROS_MAVEN_PATH=''
+export ROS_MAVEN_REPOSITORY=''
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda-11.0/lib64'
+export PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/bin:/opt/ros/noetic/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/opt/gradle/gradle-7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools:$ROS_PACKAGE_PATH"
