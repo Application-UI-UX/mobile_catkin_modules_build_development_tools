0912de0d327225fe2eb08208872c93469db93f20
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-16 10:31:39 -0400

    build

--- catkin_generated/setup_cached.sh
+++ catkin_generated/setup_cached.sh
@@ -5,14 +5,14 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export ROS_MAVEN_DEPLOYMENT_REPOSITORY=''
-export ROS_MAVEN_PATH=''
-export ROS_MAVEN_REPOSITORY=''
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/share/maven'
+export ROS_MAVEN_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/share/maven:/opt/ros/noetic/share/maven'
+export ROS_MAVEN_REPOSITORY='https://github.com/rosjava/rosjava_mvn_repo/raw/master'
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda-11.0/lib64'
-export PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/bin:/opt/ros/noetic/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/opt/gradle/gradle-7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/bin:/opt/ros/noetic/bin:/home/ronaldsonbellande-hplaptop/.local/share/nvim/lsp_servers/cmake/venv/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/opt/gradle/gradle-7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PKG_CONFIG_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PYTHONPATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/share/common-lisp'
