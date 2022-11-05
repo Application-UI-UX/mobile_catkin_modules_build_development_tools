3cef2fd364245fdabf4e8d8b9da875b6259138f0
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-11-05 14:06:27 -0400

    errors

--- catkin_generated/pkg.develspace.context.pc.py
+++ catkin_generated/pkg.develspace.context.pc.py
@@ -3,6 +3,6 @@ CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
 PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "rosjava_build_tools"
-PROJECT_SPACE_DIR = "/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel"
+PROJECT_NAME = "rosmobile_build_tools"
+PROJECT_SPACE_DIR = "/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/devel"
 PROJECT_VERSION = "0.4.0"
