cbdbe0a515fca1ab74dccf9c621f1037203ac4ed
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-11-08 08:37:39 -0500

    rosmobile

---
+++ devel/etc/catkin/profile.d/15.rosmobile.bash
@@ -0,0 +1,6 @@
+
+SCRIPT=/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/generate_environment_variables.py
+
+export ROS_MAVEN_PATH="`python ${SCRIPT} --maven-path`"
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY="`python ${SCRIPT} --maven-deployment-repository`"
+export ROS_MAVEN_REPOSITORY="`python ${SCRIPT} --maven-repository`"
