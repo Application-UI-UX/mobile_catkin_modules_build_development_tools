b9663d090892a5658eaaa067a556030ab2378a82
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-08-11 15:41:57 -0400

    latest pushes

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -5,13 +5,14 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/share/maven'
+export ROS_MAVEN_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/share/maven:/opt/ros/noetic/share/maven'
+export ROS_MAVEN_REPOSITORY='Nothing to see here - please provide one of the valid command switches.'
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/lib:$LD_LIBRARY_PATH"
-export PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/bin:$PATH"
+export LD_LIBRARY_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda-11.0/lib64:/usr/local/cuda/lib64:/usr/local/cuda-11.0/lib64'
+export PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/bin:/opt/ros/noetic/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/home/ronaldsonbellande-hplaptop/.sdkman/candidates/gradle/current/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/opt/gradle/gradle-7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PKG_CONFIG_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PYTHONPATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/lib/python3/dist-packages:$PYTHONPATH"
-export ROS_MAVEN_DEPLOYMENT_REPOSITORY='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/share/maven'
-export ROS_MAVEN_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/devel/share/maven:$ROS_MAVEN_PATH"
-export ROS_PACKAGE_PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools:/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools:/opt/ros/main/share'
+export ROS_PACKAGE_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools:$ROS_PACKAGE_PATH"
