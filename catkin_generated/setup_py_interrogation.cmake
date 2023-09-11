f563253be21e11d7cad8861b8f6605b316a22ba8
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-09-11 16:29:14 -0400

    latest pushes

--- catkin_generated/setup_py_interrogation.cmake
+++ catkin_generated/setup_py_interrogation.cmake
@@ -1,5 +1,5 @@
 set(rosmobile_build_tools_SETUP_PY_SETUP_MODULE "distutils.core")
-set(rosmobile_build_tools_SETUP_PY_VERSION "0.4.1")
+set(rosmobile_build_tools_SETUP_PY_VERSION "0.4.2")
 set(rosmobile_build_tools_SETUP_PY_SCRIPTS "scripts/catkin_create_android_pkg;scripts/catkin_create_android_project;scripts/catkin_create_android_library_project;scripts/catkin_create_rosmobile_pkg;scripts/catkin_create_rosmobile_project;scripts/catkin_create_rosmobile_library_project")
 set(rosmobile_build_tools_SETUP_PY_PACKAGES "rosmobile_build_tools")
 set(rosmobile_build_tools_SETUP_PY_PACKAGE_DIRS "src/rosmobile_build_tools")
