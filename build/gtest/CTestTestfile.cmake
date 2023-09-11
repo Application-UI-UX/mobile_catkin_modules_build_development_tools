3790b2207825509bfad880d16c7b540dcdc31500
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-09-10 21:22:33 -0400

    latest pushes

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_build_tools/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
