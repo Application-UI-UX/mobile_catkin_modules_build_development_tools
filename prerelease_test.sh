3131d69fc13231a9755faa0c3c1d931533a80b51
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2023-09-14 02:09:26 -0400

    Delete prerelease_test.sh

--- prerelease_test.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p /tmp/prerelease_job
-cd /tmp/prerelease_job
-generate_prerelease_script.py \
-  https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml \
-  noetic default ubuntu focal amd64 \
-  --custom-repo \
-    mobile_catkin_modules_build_development_tools__custom-1:git:https://github.com/Application-UI-UX/mobile_catkin_modules_build_development_tools:main \
-  --level 1 \
-  --output-dir ./
