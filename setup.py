942e108847e24f140c94708b8640b4c49bfef973
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-09-14 11:09:10 -0400

    latest pushes

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 
 from setuptools import setup, find_packages
 
+# Read the contents of README.md for long description
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="mobile_catkin_modules_build_development_tools",
     version="0.4.3",  # Updated version
     description="ROS Mobile Build Tools",
     author="Ronaldson Bellande",
     author_email="ronaldsonbellande@gmail.com",
+    long_description=long_description,  # Use the README.md contents as long description
+    long_description_content_type="text/markdown",  # Specify the content type as Markdown
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     scripts=[
@@ -40,9 +46,9 @@ setup(
         "dev": ["pytest", "pytest-cov[all]", "mypy", "black"],
     },
     project_urls={
-        "Home": "https://github.com/Application-UI-UX/mobile_catkin_modules_build_development_tools",
-        "Homepage": "https://github.com/Application-UI-UX/mobile_catkin_modules_build_development_tools",
-        "documentation": "https://github.com/Application-UI-UX/mobile_catkin_modules_build_development_tools",
-        "repository": "https://github.com/Application-UI-UX/mobile_catkin_modules_build_development_tools",
+        "Home": "https://github.com/application-ui-ux/mobile_catkin_modules_build_development_tools",
+        "Homepage": "https://github.com/application-ui-ux/mobile_catkin_modules_build_development_tools",
+        "documentation": "https://github.com/application-ui-ux/mobile_catkin_modules_build_development_tools",
+        "repository": "https://github.com/application-ui-ux/mobile_catkin_modules_build_development_tools",
     },
 )
